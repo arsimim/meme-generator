e798fe82a6a724bf3dac39beb8d6038d89ce6193
arsimim <arsimmatoshi@gmail.com>
2022-10-06 12:58:17 +0200

    first commit

---
+++ src/components/MemeApp.js
@@ -0,0 +1,14 @@
+import React from "react";
+import MemeGenerator from "./MemeGenerator";
+import Header from "./Header";
+
+const MemeApp = () => {
+  return (
+    <div>
+      <Header />
+      <MemeGenerator/>
+    </div>
+  );
+};
+
+export default MemeApp;
